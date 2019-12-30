b37364fac410bcabc1ffa3d6f6108aa2db17f581
Zachary Waxman <zwaxman@gmail.com>
2019-12-30 17:47:51 -0600

    Finished file upload and submit feature, able to parse file and extract text as string

--- client/components/InputText.js
+++ client/components/InputText.js
@@ -1,8 +1,31 @@
-import React from 'react'
+import React from "react";
 
-const InputText = () => (
-  <div>
-  </div>
-)
+class InputText extends React.Component {
+  constructor() {
+    super() 
+    this.onSubmit = this.onSubmit.bind(this)
+  }
 
-export default InputText
+  onSubmit(e) {
+    e.preventDefault()
+    const reader = new FileReader()
+    reader.onload = () => {
+      const text = reader.result
+      console.log(text)
+    }
+    reader.readAsText(e.target.file.files[0])
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.onSubmit}>
+          <input name='file' type="file" />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default InputText;
