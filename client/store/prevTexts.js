f072f0fd9c0ed250c90ccd9f2c43239124b21dce
Zachary Waxman <zwaxman@gmail.com>
2019-12-30 16:42:19 -0600

    Finished setting up initial client boilerplate

---
+++ client/store/prevTexts.js
@@ -0,0 +1,28 @@
+import axios from 'axios'
+
+/**
+ * ACTION TYPES
+ */
+
+/**
+ * INITIAL STATE
+ */
+const initialState = []
+
+/**
+ * ACTION CREATORS
+ */
+
+/**
+ * THUNK CREATORS
+ */
+
+/**
+ * REDUCER
+ */
+export default function(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
