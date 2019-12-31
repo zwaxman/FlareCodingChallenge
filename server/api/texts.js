168cf9e733d57eefd0ebb0d7fc5153023034f9f2
Zachary Waxman <zwaxman@gmail.com>
2019-12-31 11:46:38 -0600

    Wrote API routes for fetching all previous texts, posting a text, and fetching a text by id

--- server/api/texts.js
+++ server/api/texts.js
@@ -10,3 +10,21 @@ router.get('/', async (req, res, next) => {
     next(err)
   }
 })
+
+router.post('/', async (req, res, next) => {
+  try {
+    const text = await Text.create(req.body)
+    res.json(text)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    const text = await Text.findByPk(req.params.id)
+    res.json(text)
+  } catch (err) {
+    next(err)
+  }
+})
