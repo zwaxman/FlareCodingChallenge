d1c76a16fea78cac8cdd6c5c7c187ed1ab891ce8
Zachary Waxman <zwaxman@gmail.com>
2019-12-31 02:12:07 -0600

    Added auxiliary data structure to keep track of words with highest frequency

--- client/util/analyzeText.js
+++ client/util/analyzeText.js
@@ -4,6 +4,10 @@ export default (text, excludeStopWords) => {
   let indexStart = 0;
   let indexEnd = 0;
   const trie = new Trie();
+  const minNumWords = 10
+  let numWords = 0
+  let numDistinctWords = 0
+  const topWords = new TopNWords(minNumWords)
   while (indexEnd < text.length) {
     const char = text[indexEnd];
     if (!isAlpha(char)) {
@@ -11,9 +15,13 @@ export default (text, excludeStopWords) => {
         const word = removeSuffix(
           text.slice(indexStart, indexEnd).toLowerCase()
         );
+        
         if (!excludeStopWords || !stopWords.has(word)) {
+            numWords++
             const count = trie.addWord(word);
-            console.log(word, count);
+            numDistinctWords = count===1 ? numDistinctWords+1 : numDistinctWords
+            topWords.addWord(word, count)
+            console.log(topWords, numWords, numDistinctWords)
         }
       }
       indexStart = indexEnd + 1;
@@ -25,8 +33,11 @@ export default (text, excludeStopWords) => {
       text.slice(indexStart, indexEnd).toLowerCase()
     );
     if (!excludeStopWords || !stopWords.has(word)) {
+        numWords++
         const count = trie.addWord(word);
-        console.log(word, count);
+        numDistinctWords = count===1 ? numDistinctWords+1 : numDistinctWords
+        topWords.addWord(word, count)
+        console.log(topWords, numWords, numDistinctWords)
     }
   }
 };
@@ -36,14 +47,24 @@ const isAlpha = char => {
 };
 
 const endsWithSilentE = word => {
-  return /[aeiou][b-df-hj-np-tv-xz]e/.test(word.slice(word.length - 3));
+  return /([aeiou][b-df-hj-np-tv-xz])|([b-df-hj-np-tv-xz][cg])/.test(word.slice(word.length - 2));
 };
+
+const endsWithDoubleConsonant = word => {
+    return /([bdgmnprst])\1/.test(word.slice(word.length-2))
+}
 
 const removeSuffix = word => {
   const length = word.length;
   if (length >= 5) {
     if (word.slice(length - 3) === "ing") {
-      return word.slice(0, length - 3);
+        if (endsWithDoubleConsonant(word.slice(length-3))) {
+            return word.slice(0,length-4)
+        } else if (endsWithSilentE(word.slice(0,length-3))) {
+            return word.slice(0,length-3).concat('e')
+        } else {
+            return word.slice(0,length-3)
+        }
     } else if (
       word.slice(length - 3) === "ies" ||
       word.slice(length - 3) === "ied"
@@ -53,7 +74,7 @@ const removeSuffix = word => {
   }
   if (length >= 4) {
     if (word.slice(length - 2) === "ed" || word.slice(length - 2) === "es") {
-      return endsWithSilentE(word.slice(0, length - 1))
+      return endsWithSilentE(word.slice(0, length - 2))
         ? word.slice(0, length - 1)
         : word.slice(0, length - 2);
     }
@@ -78,7 +99,7 @@ class Trie {
       if (!this.trie[currentChar]) {
         this.trie[currentChar] = new Trie();
       }
-      this.trie[currentChar].count = this.trie[currentChar].count + 1;
+      this.trie[currentChar].count++;
       return this.trie[currentChar].count;
     } else {
       if (!this.trie[currentChar]) {
@@ -87,6 +108,36 @@ class Trie {
       return this.trie[currentChar].addWord(word.slice(1));
     }
   }
+}
+
+class TopNWords {
+    constructor(minNumWords) {
+        this.minNumWords = minNumWords
+        this.numWords=0
+        this.wordCounts = {}
+        this.minWordCount=1
+    }
+
+    addWord(word, count) {
+        if (this.numWords<this.minNumWords || count>=this.minWordCount) {
+            if (!this.wordCounts[count]) {
+                this.wordCounts[count]=new Set()
+            } 
+            this.wordCounts[count].add(word)
+            if (count===this.minWordCount) {
+                this.numWords++
+            } else if (count>this.minWordCount) {
+                this.wordCounts[count-1].delete(word)
+                if (count===this.minWordCount+1 && this.numWords - this.wordCounts[count-1].size === this.minNumWords) {
+                        this.numWords -= this.wordCounts[count-1].size
+                        this.minWordCount = count
+                }
+                if (this.wordCounts[count-1].size===0) {
+                    delete this.wordCounts[count-1]
+                }
+            }
+        }
+    }
 }
 
 
